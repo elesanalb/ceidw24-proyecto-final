1f0a139529f1acc514489ed74446192f2fdb8176
elena <elesanalb@gmail.com>
2024-07-01 22:35:09 +0200

    prueba_cuatro

--- js/header.js
+++ js/header.js
@@ -11,7 +11,7 @@ let headerStructure = `
     </ul>
 `
 
-header.innerHTML(headerStructure);
+header.innerHTML = headerStructure;
 /*let nav = document.createElement("nav");
 
 nav.innerHTML = headerStructure;
