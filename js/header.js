30317c84c8675b9da22b95c3b17186e1d942476a
elena <elesanalb@gmail.com>
2024-07-15 09:44:55 +0200

    revision links

--- js/header.js
+++ js/header.js
@@ -2,13 +2,14 @@
 const header = document.querySelector("header");
 const menu = document.querySelector(".menu");
 const footer = document.querySelector("footer");
-let menuSelect = ["home", "projects", "gallery", "about", "contact"];
+let menuSelect = ["home", "projects", "gallery"];
+let titleList = ["home", "My projects", "My collections"];
 let headerTitle = "";
 
 
 for(let i = 0; i < menuSelect.length; i++){
     if(header.classList.contains(menuSelect[i])){
-        headerTitle = menuSelect[i];
+        headerTitle = titleList[i];
     }
 }
 
@@ -16,7 +17,7 @@ const headerStructure = `
     <div class="frame-1"></div>
     <div class="header-title">
         <div>
-            <a href="index.html"><img class="logo" src="img/logo-bg-white.svg" alt="logo"></a>
+            <a href="index.html#"><img class="logo" src="img/logo-bg-white.svg" alt="logo"></a>
             <h2>lena qiao</h2>
         </div>
         <h1>${headerTitle}</h1>
@@ -25,8 +26,8 @@ const headerStructure = `
 
 const menuStructure = `
     <li class="${header.classList.contains("home") ? "select" : ""}"><a href="index.html#">home</a></li>
-    <li class="${header.classList.contains("about") ? "select" : ""}"><a href="index.html#about">about</a></li>
-    <li class="${header.classList.contains("contact") ? "select" : ""}"><a href="index.html#contact">contact</a></li>
+    <li><a href="index.html#about">about</a></li>
+    <li><a href="index.html#contact">contact</a></li>
     <li class="${header.classList.contains("projects") ? "select" : ""} dropdown"><a href="projects.html#">projects</a>
         <div class="dropdown-menu">
             <a href="projects.html#design">design</a>
@@ -36,7 +37,7 @@ const menuStructure = `
             
         </div>
     </li>
-    <li class="${header.classList.contains("gallery") ? "select" : ""} "><a href="gallery.html">gallery</a>
+    <li class="${header.classList.contains("gallery") ? "select" : ""} "><a href="gallery.html#">gallery</a>
     </li>
 `
 
@@ -56,12 +57,12 @@ const footerStructure = `
                 <a href="projects.html#other-projects">other</a>
             </div>
             <div>
-                <a href=""><h4>gallery</h4></a>
-                <a href="">the yellow collection</a>
-                <a href="">the flower collection</a>
-                <a href="">my art gallery</a>
-                <a href="">fashion design</a>
-                <a href="">my sketchbook</a>
+                <a href="gallery.html#"><h4>gallery</h4></a>
+                <a href="gallery.html#">the yellow collection</a>
+                <a href="gallery.html#">the flower collection</a>
+                <a href="gallery.html#art-section">my art gallery</a>
+                <a href="gallery.html#sketch-section">fashion design</a>
+                <a href="gallery.html#sketch-section">my sketchbook</a>
             </div>
         </div>
 
@@ -69,9 +70,9 @@ const footerStructure = `
             <a href="index.html"><img class="logo" src="img/logo-white.svg" alt="logo">
             <h4 class="name">lena qiao</h4>
             <div>
-                <a href=""><i class="fa-brands fa-linkedin fa-xl"></i></a>
-                <a href=""><i class="fa-brands fa-instagram fa-xl"></i></a>
-                <a href=""><i class="fa-solid fa-envelope fa-xl"></i></a>
+                <a href="https://www.linkedin.com/in/elena-sanchez-alberca/" target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-linkedin fa-xl"></i></a>
+                <a href="https://www.instagram.com/lenaqiaosana/" target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-instagram fa-xl"></i></a>
+                <a href="mailto:elesanalb@gmail.com" target="_blank" rel="noreferrer noopener"><i class="fa-solid fa-envelope fa-xl"></i></a>
             </div>
         </div>    
     </div>
